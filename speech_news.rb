8b4e6780803a75dfd93155c6feab52dcb4a1a36e
Sho Hashimoto <hashimoto@shokai.org>
2012-03-14 15:11:29 +0900

    say good morning before speech

--- speech_news.rb
+++ speech_news.rb
@@ -7,12 +7,19 @@ parser.bind(:loop, :l, 'do loop', false)
 parser.bind(:help, :h, 'show help')
 parser.comment(:speech_interval, 'speech interval (sec)', 3)
 parser.comment(:interval, 'news fetch interval (sec)', 60)
+h = Time.now.hour
+hello = (5 < h and h < 11) ? 'おはようございます' :
+  (10 < h and h < 17) ? 'こんにちは' : 'こんばんは'
+parser.comment(:before, 'before speech of news', "#{hello}、#{h}時のニュースです。")
+parser.comment(:after, 'after speech of news', "以上、#{h}時のニュースでした。")
 
 first, params = parser.parse ARGV
 if parser.has_option(:help)
   puts parser.help
   exit 1
 end
+
+system "say '#{params[:before]}'"
 
 news = News.new
 cache = Array.new
@@ -29,3 +36,5 @@ loop do
   break unless params[:loop]
   sleep params[:interval].to_f
 end
+
+system "say '#{params[:after]}'"
